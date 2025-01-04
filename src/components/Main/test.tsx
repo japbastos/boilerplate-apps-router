823ed899baf4dd3a4c692d0ac3eafca5b9f2d686
João Bastos <japbastos@gmail.com>
2025-01-04 15:53:31 -0300

    adding and config storybook

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -13,4 +13,11 @@ describe('<Main />', () => {
     //gerar snapshot
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render the colors correcttly', () => {
+    const { container } = render(<Main />)
+
+    // verifica se o background-color está correto
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
+  })
 })
